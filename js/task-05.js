c38cb1c5c74cf807149bae5d8ba9554065dab85a
Lenlen19 <143014369+Lenlen19@users.noreply.github.com>
2024-05-20 12:38:07 +0800

    Final code

---
+++ js/task-05.js
@@ -0,0 +1,6 @@
+let inputElement = document.getElementById("name-input");
+let output = document.getElementById("name-output");
+
+inputElement.addEventListener("input", function (event) {
+  output.textContent = event.target.value;
+});
